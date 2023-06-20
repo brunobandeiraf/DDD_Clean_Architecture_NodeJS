702822a44fd7b37525596c12dbf50f6f9e65b388
Bruno Bandeira <brunobandeirafernandes@gmail.com>
2023-06-20 16:39:29 -0300

    types optional

--- src/domain/use-cases/answer-question.ts
+++ src/domain/use-cases/answer-question.ts
@@ -1,5 +1,6 @@
 import { Answer } from "../entities/answer"
 import { AnswersRepository } from "../repositories/answers-repository"
+import { UniqueEntityID } from "../../core/entities/unique-entity-id"
 
 interface AnswerQuestionUseCaseRequest {
   instructorId: string
@@ -12,10 +13,11 @@ export class AnswerQuestionUseCase {
     private answersRepository: AnswersRepository,
   ) {}
 
-  async execute({ instructorId, questionId, content }: AnswerQuestionUseCaseRequest) {    const answer = new Answer({
+  async execute({ instructorId, questionId, content }: AnswerQuestionUseCaseRequest) {   
+    const answer = Answer.create({
       content,
-      authorId: instructorId,
-      questionId,
+      authorId: new UniqueEntityID(instructorId),
+      questionId: new UniqueEntityID(questionId),
     })
 
     await this.answersRepository.create(answer)
