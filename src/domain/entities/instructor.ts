48b1e4a2fc3d953b77e8ed4db87d6a6f07446bd4
Bruno Bandeira <brunobandeirafernandes@gmail.com>
2023-06-20 15:51:36 -0300

    core entities

--- src/domain/entities/instructor.ts
+++ src/domain/entities/instructor.ts
@@ -1,11 +1,7 @@
-import { randomUUID } from "node:crypto"
+import { Entity } from "../../core/entities/entity"
 
-export class Instructor {
-  public id: string
-  public name: string
-
-  constructor(name: string, id?: string) {
-    this.name = name
-    this.id = id ?? randomUUID()
-  }
+interface InstructorProps {
+  name: string
+}
+export class Instructor extends Entity<InstructorProps> {
 }
--- src/domain/entities/student.ts
+++ src/domain/entities/student.ts
@@ -1,11 +1,7 @@
-import { randomUUID } from "node:crypto"
+import { Entity } from "../../core/entities/entity"
 
-export class Student {
-  public id: string
-  public name: string
-
-  constructor(name: string, id?: string) {
-    this.name = name
-    this.id = id ?? randomUUID()
-  }
+interface StudentProps {
+  name: string
+}
+export class Student extends Entity<StudentProps> {
 }
