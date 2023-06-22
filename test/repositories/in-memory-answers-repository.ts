fc3a54c2a87989c12f4d6bcaf7628d99464b054b
Bruno Bandeira <brunobandeirafernandes@gmail.com>
2023-06-22 12:04:15 -0300

    fetch question answer

--- test/repositories/in-memory-answers-repository.ts
+++ test/repositories/in-memory-answers-repository.ts
@@ -1,5 +1,6 @@
 import { AnswersRepository } from '@/domain/forum/application/repositories/answers-repository'
 import { Answer } from '@/domain/forum/enterprise/entities/answer'
+import { PaginationParams } from '@/core/repositories/pagination-params'
 
 export class InMemoryAnswersRepository implements AnswersRepository {
   public items: Answer[] = []
@@ -14,6 +15,14 @@ export class InMemoryAnswersRepository implements AnswersRepository {
     return answer
   }
 
+  async findManyByQuestionId(questionId: string, { page }: PaginationParams) {
+    const answers = this.items
+      .filter((item) => item.questionId.toString() === questionId)
+      .slice((page - 1) * 20, page * 20)
+
+    return answers
+  }
+  
   async create(answer: Answer) {
     this.items.push(answer)
   }
@@ -23,7 +32,7 @@ export class InMemoryAnswersRepository implements AnswersRepository {
 
     this.items[itemIndex] = answer
   }
-  
+
   async delete(answer: Answer) {
     const itemIndex = this.items.findIndex((item) => item.id === answer.id)
 
