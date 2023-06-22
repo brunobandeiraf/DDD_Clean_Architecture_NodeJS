7d26447fb29680c75afe51ec4b2cdd2e4a525ef3
Bruno Bandeira <brunobandeirafernandes@gmail.com>
2023-06-22 12:54:46 -0300

    answer comments repository

--- test/repositories/in-memory-answer-comments-repository.ts
+++ test/repositories/in-memory-answer-comments-repository.ts
@@ -1,5 +1,6 @@
 import { AnswerCommentsRepository } from '@/domain/forum/application/repositories/answer-comments-repository'
 import { AnswerComment } from '@/domain/forum/enterprise/entities/answer-comment'
+import { PaginationParams } from '@/core/repositories/pagination-params'
 
 export class InMemoryAnswerCommentsRepository
   implements AnswerCommentsRepository
@@ -16,6 +17,14 @@ export class InMemoryAnswerCommentsRepository
     return answerComment
   }
 
+  async findManyByAnswerId(answerId: string, { page }: PaginationParams) {
+    const answerComments = this.items
+      .filter((item) => item.answerId.toString() === answerId)
+      .slice((page - 1) * 20, page * 20)
+
+    return answerComments
+  }
+  
   async create(answerComment: AnswerComment) {
     this.items.push(answerComment)
   }
